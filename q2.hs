d3475a3c962d6acaa5ef960ce8664b3c67150c70
bmh10 <bmhomer13@gmail.com>
2018-04-08 10:40:07 +0100

    Q2 completed

--- q2.hs
+++ q2.hs
@@ -1,13 +1,8 @@
-fib 1 = [1]
-fib 2 = [2]
-fib n = f1 ++ [(last f1) + (last (fib (n-2)))]
-  where f1 = fib (n-1)
+solve = f 4000000 3 0 1 2 
 
-
-solve = f 3 0 1 2 
-
-f 4000001 t p1 p2 = t+2
-f n   t p1 p2 = f (n+1) t' p1' p2'
+f max n t p1 p2 
+ | p2' < max = f max (n+1) t' p1' p2'
+ | otherwise = t+2 
   where
         p1' = p2
         p2' = (p1+p2)
