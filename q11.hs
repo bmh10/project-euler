bbfc2368ca9232231c7368f810717fc465553135
bmh10 <bmhomer13@gmail.com>
2018-04-21 10:06:53 +0100

    Q11 1st attempt

--- q11.hs
+++ q11.hs
@@ -19,8 +19,15 @@ grid = [[08,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,08],
 	[20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54],
 	[01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48]]
 
-f (x,y) = f' horiz
+f (x,y) = maximum [f' horiz, f' vert, f' d1, f' d2]
  where 
-   f' coords = map (\(x,y) -> grid !! y !! x) coords
+   f' coords
+     | allOk coords = foldl1 (*) $ map (\(x,y) -> grid !! y !! x) coords
+     | otherwise    = 0
+   allOk cs = all (\(x,y) -> x >= 0 && x < 20 && y >= 0 && y < 20) cs 
    horiz = [(x,y), (x+1,y), (x+2,y), (x+3,y)]
    vert  = [(x,y), (x,y+1), (x,y+2), (x,y+3)]
+   d1    = [(x,y), (x-1,y+1), (x-2,y+2), (x-3,y+3), (x-4,y+4)]
+   d2    = [(x,y), (x+1,y+1), (x+2,y+2), (x+3,y+3), (x+4,y+4)]
+
+solve = maximum [f (x,y) | x <- [0..19], y <- [0..19]]
