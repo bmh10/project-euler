56cec23388165327918733a1448e098a77fc6d26
bmh10 <bmhomer13@gmail.com>
2018-04-28 15:48:02 +0100

    Q12

--- q12.hs
+++ q12.hs
@@ -1,7 +1,14 @@
-tri 1 = 1
-tri n = n + tri (n-1)
+import Data.List
 
-divisors n = [x | x <- [1..n], n `rem` x == 0]
+solve = head $ filter ((> 500) . nDivisors) triangleNumbers
+  where 
+    nDivisors n = product $ map ((+1) . length) (group (primeFactors n))    
+    triangleNumbers = scanl1 (+) [1..]
 
-solve = map (length.divisors) $ map tri [1..10]
-
+primes = 2 : filter (null . tail . primeFactors) [3,5..]
+primeFactors n = factor n primes
+  where
+    factor n (p:ps) 
+        | p*p > n        = [n]
+        | n `mod` p == 0 = p : factor (n `div` p) (p:ps)
+        | otherwise      =     factor n ps
