a486c115f0d6d162186be8e3047182bf39a958ef
bmh10 <bmhomer13@gmail.com>
2018-05-05 09:10:04 +0100

    Q17 toWords complete

--- q17.hs
+++ q17.hs
@@ -1,4 +1,7 @@
+
+
 toWords :: Int -> String
+toWords 0 = ""
 toWords 1  = "one"
 toWords 2  = "two"
 toWords 3  = "three"
@@ -26,9 +29,13 @@ toWords 60 = "sixty"
 toWords 70 = "seventy"
 toWords 80 = "eighty"
 toWords 90 = "ninety"
+toWords 1000 = "one thousand"
 toWords n
- | n < 100 = toWords (n - m10) ++ " " ++ toWords m10
- | n > 100 = n 
+ | n < 100 = toWords (n - nm10) ++ " " ++ toWords nm10
+ | nm100 == 0 = toWords nd100 ++ " hundred" 
+ | n > 100 = toWords nd100 ++ " hundred and " ++ toWords nm100
   where 
-    m10 = (n `mod` 10)
+    nm10 = n `mod` 10
+    nd100 = n `div` 100
+    nm100 = n `mod` 100
     
