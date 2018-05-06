5f7e116120d9e10a66708c842db9176051276fda
bmh10 <bmhomer13@gmail.com>
2018-05-06 09:31:31 +0100

    Q25 done

--- q25.hs
+++ q25.hs
@@ -1,11 +1,6 @@
-fib 1 = 1
-fib 2 = 1
-fib n = fib (n-1) + fib (n-2)
-
 solve = gen [1,2]
 
-
-gen (x:y:xs)
- | (length $ show n) > 1000 = length xs + 3
- | otherwise = gen (x+y:x:y:xs)
+gen l@(x:y:xs)
+ | (length $ show n) >= 1000 = length l + 1
+ | otherwise = gen (n:l)
    where n = x+y
