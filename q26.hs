b76b150deaf8b54c50c45e94916a9930414a8b87
bmh10 <bmhomer13@gmail.com>
2018-05-07 08:56:50 +0100

    Q26 prelim

--- q26.hs
+++ q26.hs
@@ -1,6 +1,12 @@
 solve = map (\x -> 1/x) [1..1000]
 
-cycleLen n = cycleLen' n ((length n) `div` 2)
-cycleLen' n l = n 
+cycleLen n = cycleLen' n ((length $ show n) - 1)
+
+cycleLen' n 0 = 0
+cycleLen' n l = if isCycle n l then l else cycleLen' n (l-1)
+
+isCycle n l = n' == genStr s (length n')  
   where n' = show n
         s  = take l n'
+
+genStr n l = take l $ concat $ repeat n 
