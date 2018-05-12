da3f1ac73db833eaaa142d01ef7d05df62bb3ca6
bmh10 <bmhomer13@gmail.com>
2018-05-12 09:46:15 +0100

    Q206 attempt 1

---
+++ q206.hs
@@ -0,0 +1,9 @@
+import Data.List
+
+solve = find matches [x | x <- [1..]]
+
+matches x = everyOtherDigit (show (x*x)) == "1234567890"
+
+everyOtherDigit [] = []
+everyOtherDigit [a] = [a]
+everyOtherDigit (x:y:xs) = x:everyOtherDigit xs
