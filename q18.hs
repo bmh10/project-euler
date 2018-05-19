c97f0ec01a52247241d0256d5540db613e945ea1
bmh10 <bmhomer13@gmail.com>
2018-05-19 09:43:28 +0100

    Q18 part2

--- q18.hs
+++ q18.hs
@@ -6,6 +6,11 @@ f (xs:xss) = xs -- TODO
 foldDown xs1 xs2 = xs1
 
 
-ex1 = [[1], [2,3]]
+ex1 = [[[1]], [[2],[3]]]
 
-f1 (xs:xs2:xss) = map (+ (head xs)) xs2
+f1 (xs:xs2:xss) = map (add $ head xs) xs2
+
+add [x] [y] = [x+y]
+
+
+childIdx n = [n, n+1]
