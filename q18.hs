44becfa1dd7ff1be3345745a81bf34dd6ace7e36
bmh10 <bmhomer13@gmail.com>
2018-05-19 10:10:38 +0100

    Q18 part 3

--- q18.hs
+++ q18.hs
@@ -1,4 +1,4 @@
-tri = [[(3)],[(7),(4)],[(2),(4),(6)],[(8),(5),(9),(3)]]
+tri = [[[3]],[[7],[4]],[[2],[4],[6]],[[8],[5],[9],[3]]]
 
 f (xs:xss) = xs -- TODO
 
@@ -8,9 +8,12 @@ foldDown xs1 xs2 = xs1
 
 ex1 = [[[1]], [[2],[3]]]
 
-f1 (xs:xs2:xss) = map (add $ head xs) xs2
+f1 ((x:xs):xs2:xss) idx
+ -- | length xs == 1 = map (add x) xs2
+ | otherwise = [add x (xs2!!idx), add x (xs2!!(idx+1))]
 
 add [x] [y] = [x+y]
+--add xs ys = 
 
 
 childIdx n = [n, n+1]
